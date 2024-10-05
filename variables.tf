64334dbd909586c6729181ea4d2cce7d1564abb6
Felix Yanganaev <yangfell@gmail.com>
2024-10-05 10:27:38 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "account_id" {
   description = "AWS Account ID"
   type        = string
