9d222ce298b7ad6011ad82b545613130511a7a54
Felix Yanganaev <yangfell@gmail.com>
2024-10-05 21:32:26 +0300

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,45 +1,112 @@
-# GHA Role Provision via OIDC
-resource "aws_iam_openid_connect_provider" "gha_oidc" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"] # not requiredj
+# resources.tf
+
+# S3 bucket for storing Terraform state
+resource "aws_s3_bucket" "terraform_state_s3_bucket" {
+  bucket        = var.terraform_state_s3_bucket_name
+  force_destroy = true
+
+  tags = {
+    Name        = var.terraform_state_s3_bucket_name
+    Environment = var.terraform_environment
+  }
 }
 
-# Prepare OIDC policy
-data "aws_iam_openid_connect_provider" "existing_gha_oidc" {
-  url = "https://token.actions.githubusercontent.com"
+# S3 bucket versioning enable
+resource "aws_s3_bucket_versioning" "terraform_state_s3_bucket" {
+  bucket = var.terraform_state_s3_bucket_name
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
-resource "aws_iam_role" "github_actions_role" {
-  name = "GithubActionsRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+# DynamoDB table for storing Terraform locking state
+resource "aws_dynamodb_table" "terraform_state_lock_table" {
+  name         = var.terraform_state_lock_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = var.terraform_state_lock_table_name
+    Environment = var.terraform_environment
+  }
+}
+
+# IAM role used by GitHub Actions
+resource "aws_iam_role" "terraform_github_actions_role" {
+  name               = var.terraform_github_actions_role_name
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role_policy.json
+
+  tags = {
+    Name        = var.terraform_github_actions_role_name
+    Environment = var.terraform_environment
+  }
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github_actions_IODC_provider.arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:yanganaev/rsschool-devops-course-tasks:*"]
+      # values   = ["repo:yanganaev/rsschool-devops-course-tasks:ref:refs/heads/main"] # Uncomment it to make condition more strict
+    }
+  }
+}
+
+# Attach Required Policies to the IAM role
+resource "aws_iam_role_policy_attachment" "required_iam_policies" {
+  for_each   = toset(var.required_iam_policies)
+  role       = aws_iam_role.terraform_github_actions_role.name
+  policy_arn = each.value
+}
+
+# Create the custom DynamoDB access policy to let Terraform access DynamoDB table for storing Terraform locking state
+resource "aws_iam_policy" "terraform_dynamodb_access_policy" {
+  name        = var.terraform_dynamodb_access_policy_name
+  description = var.terraform_dynamodb_access_policy_name_description
+
+  policy = jsonencode({
+    Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow",
-        Principal = {
-          Federated = data.aws_iam_openid_connect_provider.existing_gha_oidc.arn
-        },
-        Action = "sts:AssumeRoleWithWebIdentity",
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
-            "token.actions.githubusercontent.com:sub": "repo:yanganaev/rsschool-test:ref:refs/heads/task_1"
-          }
-        }
-      }
+        Effect   = "Allow"
+        Action   = var.terraform_dynamodb_access_allowed_actions
+        Resource = "arn:aws:dynamodb:${var.aws_region}:${var.aws_account_id}:table/${var.terraform_state_lock_table_name}"
+      },
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "attach_policies" {
-  role       = aws_iam_role.terraform_gha_role.name
-
-  # Пример для привязки нескольких политик
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+# Attach the custom DynamoDB access policy to the IAM role
+resource "aws_iam_role_policy_attachment" "terraform_dynamodb_access" {
+  role       = aws_iam_role.terraform_github_actions_role.name
+  policy_arn = aws_iam_policy.terraform_dynamodb_access_policy.arn
 }
 
-resource "aws_iam_role_policy_attachment" "attach_ec2_policy" {
-  role       = aws_iam_role.terraform_gha_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+# GitHub Actions OIDC Provider
+resource "aws_iam_openid_connect_provider" "github_actions_IODC_provider" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
+
+  tags = {
+    Name        = var.terraform_github_actions_IODC_provider_name
+    Environment = var.terraform_environment
+  }
 }
