86a1681a70912b10c34ec0be79a1df17842bb6bc
Felix Yanganaev <yangfell@gmail.com>
2024-10-05 21:39:35 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,9 @@
-variable "gha_role" {
-  description = "IAM role used by GitHub Actions"
+variable "terraform_github_actions_IODC_provider_name" {
+  description = "Name of the GitHub Actions OIDC provider"
   type        = string
-  default     = "GithubActionsRole"
 }
 
-variable "iam_policies" {
-  description = "List of Required IAM Policies"
-  type        = list(string)
-  default = [
-    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
-    "arn:aws:iam::aws:policy/AmazonRoute53FullAccess",
-    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
-    "arn:aws:iam::aws:policy/IAMFullAccess",
-    "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
-    "arn:aws:iam::aws:policy/AmazonSQSFullAccess",
-    "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess",
-    "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess" # For future dynamodb setup of S3 backend
-  ]
+variable "terraform_environment" {
+  description = "Environment for the Terraform deployment"
+  type        = string
 }
