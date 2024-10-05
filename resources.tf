185d7faae12dc5b5b8561dbd0d1f95b0680ee273
Felix Yanganaev <yangfell@gmail.com>
2024-10-05 21:25:26 +0300

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -32,12 +32,6 @@ resource "aws_iam_role" "github_actions_role" {
   })
 }
 
-# Create Role
-resource "aws_iam_role" "terraform_gha_role" {
-  name               = "terraform-github-actions-role"
-  assume_role_policy = data.aws_iam_policy_document.oidc_policy.json
-}
-
 # Attach Policies to the role
 resource "aws_iam_role_policy_attachment" "attach_policies" {
   for_each   = toset(var.iam_policies)
